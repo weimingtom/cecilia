623e6a0517119bca1582ee6cdea33b4225cbed47
weimingtom <weimingtom@qq.com>
2017-03-22 04:46:38 +0800

    parser

---
+++ cecilia/parser/pgen.h.cs
@@ -0,0 +1,14 @@
+namespace cecilia 
+{
+	public partical class cecilia 
+	{
+
+
+		/* Parser generator interface */
+
+		//extern grammar *meta_grammar(void);
+
+		//struct _node;
+		//extern grammar *pgen(struct _node *);
+	}
+}
