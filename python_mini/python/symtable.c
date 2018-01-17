356b9ab82707a99d5201f43877aca2943fd0e7d1
weimingtom <weimingtom@qq.com>
2018-01-17 11:17:42 +0800

    mod

--- python_mini/python/symtable.c
+++ python_mini/python/symtable.c
@@ -1,25 +1,11 @@
+//20180117
 #include "python.h"
 #include "compile.h"
 #include "symtable.h"
 #include "graminit.h"
 #include "structmember.h"
 
-/* The compiler uses this function to load a PySymtableEntry object
-   for a code block.  Each block is loaded twice, once during the
-   symbol table pass and once during the code gen pass.  Entries
-   created during the first pass are cached for the second pass, using
-   the st_symbols dictionary.  
-
-   The cache is keyed by st_nscopes.  Each code block node in a
-   module's parse tree can be assigned a unique id based on the order
-   in which the nodes are visited by the compiler.  This strategy
-   works so long as the symbol table and codegen passes visit the same
-   nodes in the same order.
-*/
-
-
-PyObject *
-PySymtableEntry_New(struct symtable *st, char *name, int type, int lineno)
+PyObject *PySymtableEntry_New(struct symtable *st, char *name, int type, int lineno)
 {
 	PySymtableEntryObject *ste = NULL;
 	PyObject *k, *v;
@@ -28,7 +14,8 @@ PySymtableEntry_New(struct symtable *st, char *name, int type, int lineno)
 	if (k == NULL)
 		goto fail;
 	v = PyDict_GetItem(st->st_symbols, k);
-	if (v) {
+	if (v) 
+	{
 		Py_DECREF(k);
 		Py_INCREF(v);
 		return v;
@@ -41,35 +28,46 @@ PySymtableEntry_New(struct symtable *st, char *name, int type, int lineno)
 
 	v = PyString_FromString(name);
 	if (v == NULL)
+	{
 		goto fail;
+	}
 	ste->ste_name = v;
 	
 	v = PyDict_New();
 	if (v == NULL)
+	{
 	    goto fail;
+	}
 	ste->ste_symbols = v;
 
 	v = PyList_New(0);
 	if (v == NULL)
-	    goto fail;
+	{
+		goto fail;
+	}
 	ste->ste_varnames = v;
 
 	v = PyList_New(0);
 	if (v == NULL)
-	    goto fail;
+	{
+		goto fail;
+	}
 	ste->ste_children = v;
 
 	ste->ste_optimized = 0;
 	ste->ste_opt_lineno = 0;
 	ste->ste_lineno = lineno;
-	switch (type) {
+	switch (type) 
+	{
 	case funcdef:
 	case lambdef:
 		ste->ste_type = TYPE_FUNCTION;
 		break;
+	
 	case classdef:
 		ste->ste_type = TYPE_CLASS;
 		break;
+	
 	case single_input:
 	case eval_input:
 	case file_input:
@@ -78,26 +76,34 @@ PySymtableEntry_New(struct symtable *st, char *name, int type, int lineno)
 	}
 
 	if (st->st_cur == NULL)
+	{
 		ste->ste_nested = 0;
+	}
 	else if (st->st_cur->ste_nested 
 		 || st->st_cur->ste_type == TYPE_FUNCTION)
+	{
 		ste->ste_nested = 1;
+	}
 	else
+	{
 		ste->ste_nested = 0;
+	}
 	ste->ste_child_free = 0;
 	ste->ste_generator = 0;
 
 	if (PyDict_SetItem(st->st_symbols, ste->ste_id, (PyObject *)ste) < 0)
-	    goto fail;
-	
+	{
+		goto fail;
+	}
+
 	return (PyObject *)ste;
- fail:
+ 
+fail:
 	Py_XDECREF(ste);
 	return NULL;
 }
 
-static PyObject *
-ste_repr(PySymtableEntryObject *ste)
+static PyObject *ste_repr(PySymtableEntryObject *ste)
 {
 	char buf[256];
 
@@ -109,8 +115,7 @@ ste_repr(PySymtableEntryObject *ste)
 	return PyString_FromString(buf);
 }
 
-static void
-ste_dealloc(PySymtableEntryObject *ste)
+static void ste_dealloc(PySymtableEntryObject *ste)
 {
 	ste->ste_table = NULL;
 	Py_XDECREF(ste->ste_id);
@@ -142,38 +147,38 @@ PyTypeObject PySymtableEntry_Type = {
 	"symtable entry",
 	sizeof(PySymtableEntryObject),
 	0,
-	(destructor)ste_dealloc,                /* tp_dealloc */
-	0,                                      /* tp_print */
-	0,			               /* tp_getattr */
-	0,					/* tp_setattr */
-	0,			                /* tp_compare */
-	(reprfunc)ste_repr,			/* tp_repr */
-	0,					/* tp_as_number */
-	0,			                /* tp_as_sequence */
-	0,					/* tp_as_mapping */
-	0,					/* tp_hash */
-	0,					/* tp_call */
-	0,					/* tp_str */
-	PyObject_GenericGetAttr,		/* tp_getattro */
-	0,					/* tp_setattro */
-	0,					/* tp_as_buffer */
-	Py_TPFLAGS_DEFAULT,	                /* tp_flags */
- 	0,					/* tp_doc */
-	0,					/* tp_traverse */
-	0,					/* tp_clear */
-	0,					/* tp_richcompare */
-	0,					/* tp_weaklistoffset */
-	0,					/* tp_iter */
-	0,					/* tp_iternext */
-	0,					/* tp_methods */
-	ste_memberlist,				/* tp_members */
-	0,					/* tp_getset */
-	0,					/* tp_base */
-	0,					/* tp_dict */
-	0,					/* tp_descr_get */
-	0,					/* tp_descr_set */
-	0,					/* tp_dictoffset */
-	0,					/* tp_init */
-	0,					/* tp_alloc */
-	0,					/* tp_new */
+	(destructor)ste_dealloc,  
+	0, 
+	0,			            
+	0,		
+	0,			             
+	(reprfunc)ste_repr,		
+	0,				
+	0,			         
+	0,				
+	0,				
+	0,				
+	0,				
+	PyObject_GenericGetAttr,
+	0,				
+	0,			
+	Py_TPFLAGS_DEFAULT,
+ 	0,			
+	0,			
+	0,			
+	0,			
+	0,			
+	0,			
+	0,			
+	0,			
+	ste_memberlist,
+	0,			
+	0,			
+	0,			
+	0,			
+	0,			
+	0,			
+	0,			
+	0,			
+	0,			
 };
